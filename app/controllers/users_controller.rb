ae8e51b8259c34d7f2c66fc6e543bda862f4c22e
Colin Reinhart <colinbreinhartgmail.com>
2022-07-28 17:57:40 -0600

    Added bootstrap and some styling to landing page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,6 @@ class UsersController < ApplicationController
   def create
     auth = request.env["omniauth.auth"]
     session[:access_token] = auth[:credentials][:token]
-    binding.pry
     # user = UserFacade.create_user(auth_hash[:info])
     redirect_to '/'
   end
