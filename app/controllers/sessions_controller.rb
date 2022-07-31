215becfdc43c14ecb4868b586a68761d2bf72e20
Clay Ash <claycourtneyash@gmail.com>
2022-07-31 09:23:29 -0600

    Add sessions conditional for sign out button

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,4 +7,9 @@ class SessionsController < ApplicationController
     session[:user_id] = params[:user_id]
     redirect_to root_path
   end
+
+  def destroy 
+    session.destroy 
+    redirect_to root_path
+  end
 end
