33bfad0f3a4cd238668c4cdfff83414762b8e1da
Colin Reinhart <colinbreinhartgmail.com>
2022-07-31 12:43:57 -0600

    Links work with bootstrap styling

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   get '/auth/google_oauth2/callback', to: 'users#create'
 
-  delete '/logout', to: 'sessions#destroy'
+  get '/logout', to: 'sessions#destroy'
   # resources :users, only: [:create]
 
   # get '/buildings/index', to: 'buildings#index'
