2cab679f4523a21b9f14670705697cc540146d4b
Clay Ash <claycourtneyash@gmail.com>
2022-08-01 14:31:19 -0600

    Add new attributes for favorite poro and service

--- app/poros/favorite.rb
+++ app/poros/favorite.rb
@@ -3,12 +3,17 @@ class Favorite
               :title,
               :other_titles,
               :details,
-              :id
+              :id,
+              :location_id,
+              :pdf
   def initialize(attributes)
-    @photo        = attributes[:photo]
+    require 'pry'; binding.pry
+    @photo        = attributes[:medium_photo]
     @title        = attributes[:title]
     @other_titles = attributes[:other_titles]
     @details      = attributes[:details]
     @id           = attributes[:id]
+    @location_id  = attributes[:location_id]
+    @pdf          = attributes[:pdf]
   end
 end
--- app/services/favorite_service.rb
+++ app/services/favorite_service.rb
@@ -5,19 +5,20 @@ class FavoriteService < BaseService
     get_json(response)
   end
 
-  def self.one_favorite(user_id, photo_id)
-    response = conn.get("api/v1/users/#{user_id}/favorites/#{photo_id}")
-    get_json(response)
-  end
+  # def self.one_favorite(user_id, photo_id)
+  #   response = conn.get("api/v1/users/#{user_id}/favorites/#{photo_id}")
+  #   get_json(response)
+  # end
 
   def self.create_favorite(user_id, data)
     response = conn.post("/api/v1/users/#{user_id}/favorites", {
-      photo: data[:photo],
+      user_id: data[:user_id],
       title: data[:title],
+      photo: data[:photo],
+      location_id: data[:location_id],
       other_titles: data[:other_titles],
       details: data[:details],
-      id: data[:id],
-      user_id: user_id
+      pdf: data[:pdf]
     }.to_json, "Content-Type" => "application/json")
     get_json(response)
   end
