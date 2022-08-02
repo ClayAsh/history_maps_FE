69a750938c086e1f076ce45c53ffecca5f76e42f
Matt Deming <vballmatt44@gmail.com>
2022-08-02 08:24:12 -0600

    More API set-up

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -2,6 +2,7 @@ class BuildingsController < ApplicationController
 
   def index
     @building = BuildingFacade.find_building(params[:address])
+    binding.pry
     @properties = PropertyFacade.find_property(@building)
     # @images = ImagesFacade.find_image(@properties[])
   end
--- app/facade/property_facade.rb
+++ app/facade/property_facade.rb
@@ -1,8 +1,8 @@
 class PropertyFacade
 
   def self.find_property(location)
-    loc_results = BuildingFacade.find_building(location)
     binding.pry
+    # loc_results = BuildingFacade.find_building(location)
     PropertyService.location_results(loc_results)
   end
 end
