35dd06c9505cca69717f7fe47d9543436516bef7
Clay Ash <claycourtneyash@gmail.com>
2022-08-02 11:28:47 -0600

    Add expectations for favorite index spec and render favorite info on index page

--- spec/features/favorites/index_spec.rb
+++ spec/features/favorites/index_spec.rb
@@ -1,10 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe 'favorites index page' do 
-  xit 'shows photo and information for favorites' do 
-    #allow to return current user 
-    visit '/favorites'
+  before :each do
+    Rails.application.env_config['omniauth.auth'] = OmniAuth.config.mock_auth[:google_oauth2]
+    data = { user_id: 5,
+             title: "1623 Market Street Denver CO",
+             photo: "jpeg.url",
+             location_id: "pk id",
+             other_titles: "1225",
+             details: "some details",
+             pdf: "pdf"
+           }
+    FavoriteFacade.new_favorite(5, data)
+  end
 
-    expect(page).to have_content("")
+  it 'can navegate to user favorites and display data', :vcr do 
+    visit '/'
+
+    click_link "Register/Sign In"
+    
+    expect(current_path).to eq('/')
+
+    click_link "Favorites"
+    
+    expect(current_path).to eq('/favorites')
+
+    expect(page).to have_content("1623 Market Street Denver CO")
+    expect(page).to have_content("jpeg.url")
   end
 end
