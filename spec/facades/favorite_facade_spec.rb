5f44170132924140a9081b674f64c88507a03dc7
Clay Ash <claycourtneyash@gmail.com>
2022-08-02 17:43:55 -0600

    Adjust favorite test to display photo and pdf results

--- spec/facades/favorite_facade_spec.rb
+++ spec/facades/favorite_facade_spec.rb
@@ -23,4 +23,22 @@ RSpec.describe FavoriteFacade do
 
     expect(new_favorite).to be_a(Favorite)
   end
+
+  it 'creates poro for new favorite with nil values', :vcr do
+    user_id = 3
+    data = { user_id: user_id,
+             title: "1623 Market Street Denver CO",
+             photo: "jpeg.url",
+             location_id: "pk id",
+             other_titles: nil,
+             details: "some details",
+             pdf: nil
+           }
+   
+    new_favorite = FavoriteFacade.new_favorite(user_id, data)
+    expect(new_favorite).to be_a(Favorite)
+    expect(new_favorite.user_id).to eq(3)
+    expect(new_favorite.title).to eq("1623 Market Street Denver CO")
+    expect(new_favorite.other_titles).to eq(nil)
+  end
 end
--- spec/features/favorites/index_spec.rb
+++ spec/features/favorites/index_spec.rb
@@ -5,11 +5,11 @@ RSpec.describe 'favorites index page' do
     Rails.application.env_config['omniauth.auth'] = OmniAuth.config.mock_auth[:google_oauth2]
     data = { user_id: 5,
              title: "1623 Market Street Denver CO",
-             photo: "jpeg.url",
+             photo: "https://user-images.githubusercontent.com/99059063/182475001-5b42845f-6b32-4038-8d1b-8a75b919a470.jpg",
              location_id: "pk id",
              other_titles: "1225",
              details: "some details",
-             pdf: "pdf"
+             pdf: "https://tile.loc.gov/storage-services/master/pnp/habshaer/co/co0900/co0994/data/co0994data.pdf"
            }
     FavoriteFacade.new_favorite(5, data)
   end
@@ -22,10 +22,8 @@ RSpec.describe 'favorites index page' do
     expect(current_path).to eq('/')
 
     click_link "Favorites"
-    
-    expect(current_path).to eq('/favorites')
 
+    expect(current_path).to eq('/favorites')
     expect(page).to have_content("1623 Market Street Denver CO")
-    expect(page).to have_content("jpeg.url")
   end
 end
