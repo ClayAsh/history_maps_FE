68246fecf8729c26e62e5429d8ac5614b96df82e
Colin Reinhart <colinbreinhartgmail.com>
2022-08-04 11:02:19 -0600

    Add to favorites

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,9 +2,9 @@ class FavoritesController < ApplicationController
   def new
   end
 
-  def create(data)
-    session[:user_id] = user_id
-    FavoriteFacade.new_favorite(user_id, data)
+  def create
+    user_id = session[:user_id]
+    FavoriteFacade.new_favorite(user_id.to_i, favorite_params)
     redirect_to '/favorites'
   end
 
@@ -12,4 +12,9 @@ class FavoritesController < ApplicationController
     @session_user_id = session[:user_id]
     @favorites = FavoriteFacade.get_favorites(@session_user_id)
   end
+
+  private
+    def favorite_params
+      params.permit(:title, :thumbnail, :short_id, :creator, :published_date, :image_full)
+    end
 end
